cd39b9dc9da34ca3e1a381b4240f30f51bfdf6ec
Cleave <cleavestone94@gmail.com>
2024-07-01 23:54:03 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical-chatbot",
+    version='0.0.0',
+    author='Cleave Adungo',
+    author_email='cleavestone94@gmail.com',
+    packages=find_packages()
+    
+)
