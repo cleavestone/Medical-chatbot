2f45dd38fdedb28427ddc1b40b8fdbfd6dc430b5
Cleave <cleavestone94@gmail.com>
2024-07-04 00:54:53 +0300

    static files updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request
 from langchain.vectorstores import Pinecone
 import pinecone
 from langchain_pinecone import PineconeVectorStore
@@ -9,47 +9,42 @@ from dotenv import load_dotenv
 from src.prompt import *
 from langchain.embeddings import OpenAIEmbeddings
 import os
-from pinecone.grpc import PineconeGRPC as Pinecone
-from pinecone import ServerlessSpec
+from store_index import vectorstore_from_docs
 
-app=Flask(__name__)
+app = Flask(__name__)
 load_dotenv()
 
-llm=OpenAI()
-embeddings=OpenAIEmbeddings()
+llm = OpenAI()
+embeddings = OpenAIEmbeddings()
 
-api_key=os.getenv("OPENAI_API_KEY")
-pinecone_key=os.getenv("PINECONE_API_KEY")
-os.environ["OPENAI_API_KEY"]=api_key
-os.environ["PINECONE_API_KEY"]=pinecone_key
+api_key = os.getenv("OPENAI_API_KEY")
+pinecone_key = os.getenv("PINECONE_API_KEY")
+os.environ["OPENAI_API_KEY"] = api_key
+os.environ["PINECONE_API_KEY"] = pinecone_key
 
-index_name="chatbot2"
-pc = Pinecone()
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
-embeddings=OpenAIEmbeddings()
-vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
-docsearch=vectorstore.from_existing_index(index_name=index_name,embedding=embeddings)
-
-PROMPT=PromptTemplate(template=prompt_template,input_variables=["context","question"])
-llm=OpenAI()
-
-qa=RetrievalQA.from_chain_type(
+qa = RetrievalQA.from_chain_type(
     llm=llm,
-    chain_type='stuff',
-    retriever=docsearch.as_retriever(),
+    chain_type="stuff",
+    retriever=vectorstore_from_docs.as_retriever(),
     return_source_documents=True
 )
 
-# create default route
 @app.route('/')
 def index():
     return render_template('chat.html')
 
+@app.route("/get", methods=["POST"])
+def chat():
+    msg = request.form.get('msg', '')
+    if not msg:
+        return jsonify({"error": "No message provided"}), 400
+    
+    result = qa({'query': msg})
+    return jsonify({"result": result['result']})
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
-
-
 
 
